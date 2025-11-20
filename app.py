0c3c72a091a566068a7cca47bbb55ab30d335b98
Claudio577 <chideki.yoshida@gmail.com>
2025-11-20 06:52:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.schema import HumanMessage
 
 st.title("Demo LangChain + Streamlit + GitHub 🚀")
 
-# Chave vem do Streamlit Secrets
+# chave via secrets
 api_key = st.secrets["OPENAI_API_KEY"]
 
 llm = ChatOpenAI(
-    openai_api_key=api_key,
+    api_key=api_key,
     model="gpt-4o-mini"
 )
 
@@ -16,7 +16,7 @@ user_input = st.text_input("Digite sua pergunta:")
 
 if st.button("Enviar"):
     if user_input:
-        resposta = llm([HumanMessage(content=user_input)])
+        resposta = llm.invoke([HumanMessage(content=user_input)])
         st.write(resposta.content)
     else:
         st.warning("Digite algo antes de enviar!")
