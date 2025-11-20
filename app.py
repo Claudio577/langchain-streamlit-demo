f1923a30b4d244d3f37b7bf424fc98e433781a80
Claudio577 <chideki.yoshida@gmail.com>
2025-11-20 07:08:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_core.messages import HumanMessage
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 
 st.title("RAG + LangChain + Streamlit + GitHub 📄🚀")
 
